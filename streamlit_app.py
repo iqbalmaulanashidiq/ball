f37d71dc301fc3d96dda833bdc56cedc717521d6
iqbalmaulanashidiq <iqbalmaulanashidiq@gmail.com>
2025-12-27 16:42:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import math
-import matplotlib.pyplot as plt   # ✅ FIX UTAMA
+import matplotlib.pyplot as plt   
 
 # =============================
 # CUSTOM CSS
